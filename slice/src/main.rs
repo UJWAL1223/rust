151593e0d04b0f7cf85b647f4e56bdcb4af0f950
Ujjwal KC <kcujwal98@gmail.com>
2025-03-10 18:09:16 +0545

    Slice

---
+++ slice/src/main.rs
@@ -0,0 +1,19 @@
+fn main(){
+
+    let s = String::from("Hello world");
+
+    firstWord(&s);
+}
+
+fn firstWord(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
