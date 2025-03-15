fb65529d666174932a55676fbe1b72353811c295
Ujjwal KC <kcujwal98@gmail.com>
2025-03-15 23:37:56 +0545

    stuct example

---
+++ matchExpression/src/main.rs
@@ -0,0 +1,31 @@
+#[derive(Debug)]
+enum UsStates{
+    Alaaabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsStates),
+}
+
+fn value_incents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quater from {state:?}");
+            25
+        }
+    }
+}
+
+fn main(){
+
+}
